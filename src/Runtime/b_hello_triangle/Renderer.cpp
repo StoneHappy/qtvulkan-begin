2b120c75c2157d6d6a6bdb895c4f3aa3bf6a3364
StoneHappy <18017158729@163.com>
2022-11-20 23:16:11 +0800

    ✨ feat: Vertex

--- src/Runtime/b_hello_triangle/Renderer.cpp
+++ src/Runtime/b_hello_triangle/Renderer.cpp
@@ -1,11 +1,16 @@
 #include "Renderer.h"
 #include <glm/glm.hpp>
+#include <array>
+#include <cstddef>
 namespace b_hello_triangle
 {
 	struct Vertex
 	{
 		glm::vec2 pos;
 		glm::vec3 color;
+
+		static VkVertexInputBindingDescription getBindingDescription();
+		static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions();
 	};
 
 	Renderer::Renderer(QVulkanWindow* w)
@@ -16,18 +21,20 @@ namespace b_hello_triangle
 	void Renderer::initResources()
 	{
 		qDebug("initResources");
-
-	}
-
-	void Renderer::initSwapChainResources()
-	{
-
 		VkDevice dev = m_window->device();
 		m_devFuncs = m_window->vulkanInstance()->deviceFunctions(dev);
 
 		VkBufferCreateInfo bufferInfo;
 		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
 		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+
+	}
+
+	void Renderer::initSwapChainResources()
+	{
+
+		
 
 	}
 
@@ -41,5 +48,25 @@ namespace b_hello_triangle
 
 	void Renderer::startNextFrame()
 	{
+	}
+	VkVertexInputBindingDescription Vertex::getBindingDescription()
+	{
+		VkVertexInputBindingDescription bindingDescription{};
+		bindingDescription.binding = 0;
+		bindingDescription.stride = sizeof(Vertex);
+		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+	}
+	std::array<VkVertexInputAttributeDescription, 2> Vertex::getAttributeDescriptions()
+	{
+		std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+		attributeDescriptions[0].binding = 0;
+		attributeDescriptions[0].location = 0;
+		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+		attributeDescriptions[1].binding = 0;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
 	}
 }
